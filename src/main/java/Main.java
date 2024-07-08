acde1cdb4f5ede058cc3adef344cc8db6dfcb711
Vera Knyazeva <verashibanova@yandex.ru>
2024-07-08 15:59:09 +0500

    add threadPool + callableTask

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,21 @@
 import java.util.*;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 public class Main {
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
         String[] texts = new String[25];
-        List<Thread> threads = new ArrayList<>();
+        final ExecutorService threadPool = Executors.newFixedThreadPool(texts.length);
+        final List<Future<Integer>> futures = new ArrayList<>();
         for (int i = 0; i < texts.length; i++) {
             texts[i] = generateText("aab", 30_000);
         }
         long startTs = System.currentTimeMillis(); // start time
         for (String text : texts) {
-            Thread thread = new Thread(() -> {
+            Future<Integer> future = threadPool.submit(() -> {
                 int maxSize = 0;
                 for (int i = 0; i < text.length(); i++) {
                     for (int j = 0; j < text.length(); j++) {
@@ -30,16 +35,25 @@ public class Main {
                     }
                 }
                 System.out.println(text.substring(0, 100) + " -> " + maxSize);
+                return maxSize;
             });
-            threads.add(thread);
-            thread.start();
+
+            futures.add(future);
         }
-        for (Thread thread : threads) {
-            thread.join();
+
+        int max = 0;
+        for (Future<Integer> future : futures) {
+            Integer maxByOneText = future.get();
+            if (max < maxByOneText) {
+                max = maxByOneText;
+            }
         }
+
         long endTs = System.currentTimeMillis(); // end time
 
         System.out.println("Time: " + (endTs - startTs) + "ms");
+
+        System.out.println("Maximum is: " + max);
     }
 
     public static String generateText(String letters, int length) {
